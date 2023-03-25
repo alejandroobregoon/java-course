639d8bbbc2d49524c1939577b8c4a2137cc2a5ae
Alejandro Obregón <93755183+alejandroobregoon@users.noreply.github.com>
2023-03-24 22:10:17 -0500

    agregar features to routes
    - Ok

---
+++ src/main/java/com/alejandro/routes.java
@@ -0,0 +1,8 @@
+package com.alejandro;
+
+public class routes {
+    public static void main(String[] args) {
+        System.out.println("¡Esto es una !");
+        System.out.println("Route 1---*");
+    }
+}
