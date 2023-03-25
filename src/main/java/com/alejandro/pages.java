9d439028ce5e1b12e4557c4e566d6898eb423be3
Alejandro Obregón <93755183+alejandroobregoon@users.noreply.github.com>
2023-03-24 22:25:33 -0500

    agregado nueva line og pages
    - todo ok

--- src/main/java/com/alejandro/pages.java
+++ src/main/java/com/alejandro/pages.java
@@ -3,5 +3,6 @@ package com.alejandro;
 public class pages {
     public static void main(String[] args) {
         System.out.println("This is a page");
+        System.out.println("page 1");
     }
 }
